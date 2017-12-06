aeb968b7a5c431f091461cb93c0b2e08bbfca8ae
fonzied <you.dunno@gmail.com>
2017-12-06 08:51:01 -0500

    whatever

--- bot.js
+++ bot.js
@@ -5,21 +5,18 @@ const fs = require('fs');
 let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
 require('events').EventEmitter.prototype._maxListeners = 100;
 
-// Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
   colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
+// Bot start
 var bot = new Discord.Client({
   autorun: true,
-  token: "Mzg0NDgyODUwMDgwMjkyODc1.DPzd9A.sP3Gv6Fp7E_NCru9kxmXKBflnUw"
+  token: auth.token
 });
 bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
+  console.log(`Connected as ${bot.username} - ${bot.id}`)
 });
 // User Triggered Messages
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -35,6 +32,48 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         //Create a stream to your file and pipe it to the stream
         //Without {end: false}, it would close up the stream, so make sure to include that.
         fs.createReadStream('metalgear.wav').pipe(stream, { end: false });
+        //The stream fires `done` when it's got nothing else to send to Discord.
+        stream.on('done', function () {
+          bot.leaveVoiceChannel(voiceChannelID);
+        });
+      });
+    });
+  }
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+  if (message === "!chill") {
+    let voiceChannelID = "379446728283389953"
+    bot.joinVoiceChannel(voiceChannelID, function (error, events) {
+      //Check to see if any errors happen while joining.
+      if (error) return console.error(error);
+      //Then get the audio context
+      bot.getAudioContext(voiceChannelID, function (error, stream) {
+        //Once again, check to see if any errors exist
+        if (error) return console.error(error);
+        //Create a stream to your file and pipe it to the stream
+        //Without {end: false}, it would close up the stream, so make sure to include that.
+        fs.createReadStream('glacius.mp3').pipe(stream, { end: false });
+        //The stream fires `done` when it's got nothing else to send to Discord.
+        stream.on('done', function () {
+          bot.leaveVoiceChannel(voiceChannelID);
+        });
+      });
+    });
+  }
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+  if (message === "!stop") {
+    let voiceChannelID = "379446728283389953"
+    bot.joinVoiceChannel(voiceChannelID, function (error, events) {
+      //Check to see if any errors happen while joining.
+      if (error) return console.error(error);
+      //Then get the audio context
+      bot.getAudioContext(voiceChannelID, function (error, stream) {
+        //Once again, check to see if any errors exist
+        if (error) return console.error(error);
+        //Create a stream to your file and pipe it to the stream
+        //Without {end: false}, it would close up the stream, so make sure to include that.
+        fs.createReadStream('stop.mp3').pipe(stream, { end: false });
         //The stream fires `done` when it's got nothing else to send to Discord.
         stream.on('done', function () {
           bot.leaveVoiceChannel(voiceChannelID);
@@ -139,14 +178,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     });
   }
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-  if (message === "!kids") {
-    bot.sendMessage({
-      to: channelID,
-      message: `Look at all these fucking kids.`
-    })
-  }
-});
+
 bot.on('message', function (user, userID, channelID, message, evt) {
   if (message === "!memecount") {
     bot.sendMessage({
@@ -159,26 +191,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
   if (message === "!help") {
     bot.sendMessage({
       to: channelID,
-      message: `commands are: !kids, !flagg, !piss, !loog, !ping, !chad, !slurp, !eelio, !bob, !gear (only works in jonezone), !meme, !memecount`
+      message: `commands are: !chill, !stop !loog, !chad, !bob, !gear (only works in jonezone), !meme, !memecount`
     })
   }
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-  if (message === "!ping") {
-    bot.sendMessage({
-      to: channelID,
-      message: `Pong ching chong my name is Flagg and my racism is loud like a gong!`
-    })
-  }
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-  if (message === "!flagg") {
-    bot.sendMessage({
-      to: channelID,
-      message: `John Flagg has nice tight boyhole.`
-    })
-  }
-});
+
 bot.on('message', function (user, userID, channelID, message, evt) {
   if (message === "!slurp") {
     bot.sendMessage({
@@ -187,30 +204,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     })
   }
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-  if (message === "!eelio") {
-    bot.sendMessage({
-      to: channelID,
-      message: `I'm not really a fan of jews`
-    })
-  }
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-  if (message === "!piss") {
-    bot.sendMessage({
-      to: channelID,
-      message: `HOOOOOOO PISS`
-    })
-  }
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-  if (message === "!obama") {
-    bot.sendMessage({
-      to: channelID,
-      message: `Can you believe that Obama called Elizabeth Warren 'Pocahontas' at a ceremony with native americans?`
-    })
-  }
-});
+
 bot.on('message', function (user, userID, channelID, message, evt) {
   if (message === "!memes") {
     bot.sendMessage({
